7e4b8d76d039767a1cf3daee0a5be163594212a8
michael-fares-lrn <mjfares@gmail.com>
2026-01-09 09:48:32 -0600

    extract note list into own component

---
+++ src/components/scaleInfoBar/NoteList.tsx
@@ -0,0 +1,65 @@
+import classNames from "classnames";
+import Accidental from "../icons/Accidental";
+
+function NoteList({
+    uniqueNotesForForDisplay,
+    scale,
+}: {
+    uniqueNotesForForDisplay: NoteObject[];
+    scale: Scale;
+}) {
+    return (
+        <ul className="notes-list">
+            {uniqueNotesForForDisplay.map((note: NoteObject, index: number) => {
+                const isDescendingVariantNote =
+                    uniqueNotesForForDisplay.indexOf(note) + 1 ===
+                    scale.descendingScaleVariantDegree;
+                const stringAccidental = note.name.substring(1);
+                const isNatural = note.name.length === 1;
+                const isFlat = stringAccidental === "b";
+                const isHalfFlat = stringAccidental === "-hf";
+                const isFlatHalfFlat = stringAccidental === "b-hf";
+                const noteLetter = note.name.charAt(0);
+                let jsx;
+                if (isNatural) {
+                    jsx = <>{note.name}</>;
+                } else if (isFlat) {
+                    jsx = (
+                        <>
+                            {noteLetter}
+                            <Accidental type="flat" />
+                        </>
+                    );
+                } else if (isHalfFlat) {
+                    jsx = (
+                        <>
+                            {noteLetter}
+                            <Accidental type="half-flat" />
+                        </>
+                    );
+                } else if (isFlatHalfFlat) {
+                    jsx = (
+                        <>
+                            {noteLetter}
+                            <Accidental type="half-flat" />
+                            <Accidental type="flat" />
+                        </>
+                    );
+                }
+                return (
+                    <li
+                        data-note-name={note.name}
+                        data-octave={note.octave}
+                        className={classNames("note-pill", {
+                            "descending-variant": isDescendingVariantNote,
+                        })}
+                        key={index}
+                    >
+                        {jsx}
+                    </li>
+                );
+            })}
+        </ul>
+    );
+}
+export default NoteList;
--- src/components/scaleInfoBar/ScaleInfoBar.tsx
+++ src/components/scaleInfoBar/ScaleInfoBar.tsx
@@ -1,8 +1,7 @@
 import "./scaleInforBar.css";
 import { NOTE_VALUES } from "../../constants";
 import { getNotesToPlay, uppercase } from "../../utils";
-import classNames from "classnames";
-import Accidental from "../icons/Accidental";
+import NoteList from "./NoteList";
 
 interface ScaleInfoBarProps {
     audioManager: {
@@ -122,59 +121,7 @@ function ScaleInfoBar({
                 )}
             </p>
             <div className="notes-info">
-                <p>Notes: </p>
-                <ul className="notes-list">
-                    {uniqueNotesForForDisplay.map((note, index: number) => {
-                        const isDescendingVariantNote =
-                            uniqueNotesForForDisplay.indexOf(note) + 1 ===
-                            scale.descendingScaleVariantDegree;
-                        const stringAccidental = note.name.substring(1);
-                        const isNatural = note.name.length === 1;
-                        const isFlat = stringAccidental === "b";
-                        const isHalfFlat = stringAccidental === "-hf";
-                        const isFlatHalfFlat = stringAccidental === "b-hf";
-                        const noteLetter = note.name.charAt(0);
-                        let jsx;
-                        if (isNatural) {
-                            jsx = <>{note.name}</>;
-                        } else if (isFlat) {
-                            jsx = (
-                                <>
-                                    {noteLetter}
-                                    <Accidental type="flat" />
-                                </>
-                            );
-                        } else if (isHalfFlat) {
-                            jsx = (
-                                <>
-                                    {noteLetter}
-                                    <Accidental type="half-flat" />
-                                </>
-                            );
-                        } else if (isFlatHalfFlat) {
-                            jsx = (
-                                <>
-                                    {noteLetter}
-                                    <Accidental type="half-flat" />
-                                    <Accidental type="flat" />
-                                </>
-                            );
-                        }
-                        return (
-                            <li
-                                data-note-name={note.name}
-                                data-octave={note.octave}
-                                className={classNames("note-pill", {
-                                    "descending-variant":
-                                        isDescendingVariantNote,
-                                })}
-                                key={index}
-                            >
-                                {jsx}
-                            </li>
-                        );
-                    })}
-                </ul>
+                <NoteList uniqueNotesForForDisplay={uniqueNotesForForDisplay} scale={scale}/>
             </div>
         </div>
     );
