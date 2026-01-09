3ff8bd17b853fbc25d210b00bb59a8076c5be174
michael-fares-lrn <mjfares@gmail.com>
2026-01-09 15:47:10 -0600

    question mark icons for buttons

--- src/components/description/compareButton/CompareButton.tsx
+++ src/components/description/compareButton/CompareButton.tsx
@@ -1,47 +1,49 @@
 import "./compareButton.css";
 import classNames from "classnames";
+import QuestionMark from "../../icons/QuestionMark";
 
 interface CompareButtonProps {
-	showsScale: ComparisonWesternScale | ComparisonMaqam;
-	comparingScaleName: ComparisonWesternScale | ComparisonMaqam | null;
-	setComparingScaleName: (
-		value: ComparisonWesternScale | ComparisonMaqam | null
-	) => void;
+    showsScale: ComparisonWesternScale | ComparisonMaqam;
+    comparingScaleName: ComparisonWesternScale | ComparisonMaqam | null;
+    setComparingScaleName: (
+        value: ComparisonWesternScale | ComparisonMaqam | null
+    ) => void;
 }
 
 function CompareButton({
-	showsScale,
-	comparingScaleName,
-	setComparingScaleName,
+    showsScale,
+    comparingScaleName,
+    setComparingScaleName,
 }: CompareButtonProps) {
-	const handleComparisonClick = () => {
-		if (
-			!comparingScaleName ||
-			(comparingScaleName &&
-				comparingScaleName !==
-					showsScale)
-		) {
-			setComparingScaleName(
-				showsScale as
-					| ComparisonWesternScale
-					| ComparisonMaqam
-			);
-		} else {
-			setComparingScaleName(null);
-		}
-	};
-	return (
-		<button
-			className={classNames({
-				"comparison-button": true,
-				"active": !!comparingScaleName && comparingScaleName === showsScale
-			})}
-			data-comparing-scale-name={showsScale}
-			onClick={handleComparisonClick}
-		>
-			{showsScale}
-		</button>
-	);
+    const handleComparisonClick = () => {
+        if (
+            !comparingScaleName ||
+            (comparingScaleName && comparingScaleName !== showsScale)
+        ) {
+            setComparingScaleName(
+                showsScale as ComparisonWesternScale | ComparisonMaqam
+            );
+        } else {
+            setComparingScaleName(null);
+        }
+    };
+    return (
+        <button
+            className={classNames({
+                "comparison-button": true,
+                active:
+                    !!comparingScaleName && comparingScaleName === showsScale,
+            })}
+            data-comparing-scale-name={showsScale}
+            onClick={handleComparisonClick}
+        >
+            <span>
+                {showsScale}
+
+                <QuestionMark />
+            </span>
+        </button>
+    );
 }
 
 export default CompareButton;
